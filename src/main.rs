df8bed4131d8a7ceb78633eeacb108ba592b64ff
lucianothecoder06 <lucianooscar54@gmail.com>
2026-01-05 21:15:51 -0400

    Initialize project structure with Cargo configuration and main application logic

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // Definimos los separadores y datos
+    let separator = "=".repeat(40);
+    
+    println!("{}", separator);
+    println!(" PROYECTO FINAL SISTEMAS OPERATIVOS");
+    println!("{}", separator);
+    
+    // RECUERDA: Cambia estos datos por los tuyos reales como pide el requisito [cite: 90]
+    println!("Nombre: Oscar Luciano"); // Pon aquí tu nombre real
+    println!("ID: 10153287");                // Pon aquí tu ID real
+    println!("Correo: onlp0001@ce.pucmm.edu.do");
+    
+    println!("{}", separator);
+    
+    // Opcional: Una pausa para que si se ejecuta por doble clic no se cierre inmediato
+    // (Similar a input() en Python, pero no estrictamente necesario para el servicio)
+}
